a36181062dca5958b3282bfc0beb2b07d5f903fd
phin01 <phin01@gmail.com>
2023-04-28 20:51:07 -0300

    include second test for calculateFV

--- test/calculate-values.test.js
+++ test/calculate-values.test.js
@@ -1,9 +1,17 @@
 const Calculator = require('../src/calculate-values');
 
-test('Only one payment = equal to Future Value', () => {
+test('Only one payment => FV equal to Present Value', () => {
     // Operation
     const amount = Calculator.calculateFV(100, 0.0175, 1);
 
     // Expected result
     expect(amount).toBe(100);
+});
+
+test('4 payments => FV = PV with interest', () => {
+    // Operation
+    const amount = Calculator.calculateFV(500, 0.025, 4);
+
+    // Expected result
+    expect(amount).toBe(538.45);
 });

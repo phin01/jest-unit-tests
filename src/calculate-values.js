3589430014322c3a3e8c445106fda8b986e57277
phin01 <phin01@gmail.com>
2023-04-29 17:17:05 -0300

    create calculatePmt function for payments

--- src/calculate-values.js
+++ src/calculate-values.js
@@ -12,7 +12,22 @@ function roundValue(amount) {
     return roundedValue;
 }
 
+function calculatePmt(amount, numberOfPayments) {
+    const basePayment = roundValue(roundValue(amount) / numberOfPayments);
+    const payments = Array(numberOfPayments).fill(basePayment);
+
+    //check if any rounding left over needs to be adjusted in the first payment
+    const leftOverRounding = roundValue(amount - payments.reduce((a, b) => roundValue(a + b), 0));
+    if(leftOverRounding != 0) {
+        payments[0] = roundValue(payments[0] + leftOverRounding);
+    }
+
+    return payments;
+
+}
+
 module.exports = { 
     calculateFV,
-    roundValue
+    roundValue,
+    calculatePmt
 };
